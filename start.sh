853f68a86de238aa481521ef9d68db1524cbd737
ndanhkiet11 <ct.winak11@gmail.com>
2025-09-30 21:18:29 +0700

    fix some bugs

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Create required directories
+mkdir -p /tmp/nginx /var/log/nginx /tmp/client_body /tmp/proxy /tmp/fastcgi /tmp/uwsgi /tmp/scgi
+
+# Replace ${PORT} in nginx.conf
+envsubst '${PORT}' < /app/nginx.conf > /tmp/nginx.conf
+
 # Start PHP-FPM in the background
-php-fpm -y /workspace/php-fpm.conf -F &
+php-fpm -y /app/php-fpm.conf -F &
 
 # Start Nginx in the foreground
-nginx -c /workspace/nginx.conf -g 'daemon off;'
+nginx -c /tmp/nginx.conf -g 'daemon off;'
